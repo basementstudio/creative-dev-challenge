99605ccce411dd0139349cf88a51a8feb8accc4b
Francisco Alamino <falamino00@gmail.com>
2023-05-16 12:22:20 -0300

    Initial commit

---
+++ src/components/common/welcome/index.tsx
@@ -0,0 +1,62 @@
+import s from './welcome.module.css'
+
+export const Welcome = () => {
+  return (
+    <div className={s.welcome}>
+      <div className={s.container}>
+        <div className={s.box}>
+          <p>
+            👋 Hi there. You are on the basement <code>next-typescript</code>{' '}
+            starter.
+          </p>
+        </div>
+
+        <h3>Links</h3>
+        <div className={s.box}>
+          <p>
+            📝 You can find some utils on our gists notion.&nbsp;
+            <a
+              target="_blank"
+              href="https://basementstudio.notion.site/09383099eb9d415b952af95d5e6a82cc?v=a5d4e739a25a4c3193d7f16e87bdf94e"
+              rel="noreferrer"
+            >
+              Check it out!
+            </a>
+          </p>
+        </div>
+
+        <h3>Utils</h3>
+        <div className={s.box}>
+          <p>
+            🔎 Try pressing <code>ctrl+i</code> or <code>alt+i</code> to inspect
+            boxes. Super useful for detecting overflows.
+          </p>
+        </div>
+
+        <h3>Notes</h3>
+        <div className={s.box}>
+          <p>
+            💣 Pssst... You should delete this welcome component, it's under
+            &nbsp;
+            <code>./src/components/common/welcome</code>.
+          </p>
+        </div>
+
+        <h3>Credits</h3>
+        <div className={s.box}>
+          <p>
+            From the{' '}
+            <a
+              href="https://basement.studio"
+              target="_blank"
+              rel="noopener"
+              style={{ fontWeight: 'bold', textDecoration: 'none' }}
+            >
+              basement.
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
